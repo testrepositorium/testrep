c7e06a30d111e7cf2d9b3a2f1aa54710223739de
testrepositorium <105123591+testrepositorium@users.noreply.github.com>
2022-05-12 01:09:58 +0300

    yep, useless

--- firsttask/cancel.php
+++
@@ -1,7 +0,0 @@
-<?
-$id = $_GET['id'];
-$mysqli=new mysqli('localhost', 'root', '', 'tasks');
-$mysqli->query("UPDATE first_table SET status = 1 WHERE id = '$id'"); // Выполняем задачу, меняя у нее статус
-echo "<meta http-equiv='Refresh' content='0; URL=/index.php'>";   
-$mysqli->close();
-?>
